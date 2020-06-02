ea969a6eebae073143910f90aa71456ef4f2c324
jinwen <jinwen.tay@u.nus.edu>
2020-06-02 23:55:03 +0800

    New commit

--- lib/src/pages/index.dart
+++ lib/src/pages/index.dart
@@ -28,7 +28,7 @@ class IndexState extends State<IndexPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Agora Flutter QuickStart'),
+        title: Text('WeOKE!'),
       ),
       body: Center(
         child: Container(
