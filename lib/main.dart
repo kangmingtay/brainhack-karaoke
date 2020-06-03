45f92b0e69c4c35e05706474cd370cbd2f6c1934
termehlee <e0324346@u.nus.edu>
2020-06-03 23:18:40 +0800

    Add firebase for ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
-import 'package:agora_flutter_quickstart/src/pages/songconsole.dart';
 import 'package:flutter/material.dart';
 import './src/pages/homePage.dart';
 
 void main() => runApp(MaterialApp(home: HomePage()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: IndexPage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: IndexPage(),
+//     );
+//   }
+// }
